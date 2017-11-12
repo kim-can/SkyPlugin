80c9191ec0dbbd7137d232f95c809a855a00bb8f
金灿 <c.jin@hnair.com>
2017-11-13 01:20:53 +0800

    修复

--- src/com/jincanshen/android/skygson/ThirdParty/json/java/src/org/json/JSONArray.java
+++ src/com/jincanshen/android/skygson/ThirdParty/json/java/src/org/json/JSONArray.java
@@ -36,7 +36,7 @@ import java.util.Map;
 /**
  * A JSONArray is an ordered sequence of values. Its external text form is a
  * string wrapped in square brackets with commas separating the values. The
- * internal form is an object having <code>get</code> and <code>opt</code>
+ * fileTemplates.internal form is an object having <code>get</code> and <code>opt</code>
  * methods for accessing the values by index, and <code>put</code> methods for
  * adding or replacing values. The values can be any of these types:
  * <code>Boolean</code>, <code>JSONArray</code>, <code>JSONObject</code>,
--- src/com/jincanshen/android/skyplugin/SkyCreateFileCodeCreator.java
+++ src/com/jincanshen/android/skyplugin/SkyCreateFileCodeCreator.java
@@ -1,7 +1,9 @@
 package com.jincanshen.android.skyplugin;
 
 import com.intellij.codeInsight.actions.ReformatCodeProcessor;
-import com.intellij.ide.highlighter.JavaFileType;
+import com.intellij.ide.fileTemplates.FileTemplate;
+import com.intellij.ide.fileTemplates.FileTemplateManager;
+import com.intellij.ide.fileTemplates.FileTemplateUtil;
 import com.intellij.ide.highlighter.XmlFileType;
 import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.project.Project;
@@ -10,6 +12,8 @@ import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.*;
 import com.intellij.psi.codeStyle.JavaCodeStyleManager;
 import com.oracle.tools.packager.Log;
+
+import java.util.Properties;
 
 /**
  * Created by sky on 2017/6/16.
@@ -72,8 +76,8 @@ public class SkyCreateFileCodeCreator extends WriteCommandAction.Simple {
 				}
 				xmlName = "activity_" + xmlName.toLowerCase();
 
-				view = generateView(viewName, xmlName, bizName, "jc.sky.view.SKYActivity");
 				xml = gennerateXml(xmlName);
+				view = generateView(viewName, xmlName, bizName, "jc.sky.view.SKYActivity");
 				break;
 			case 1: // fragment
 				if (!viewName.endsWith("Fragment")) {
@@ -81,8 +85,8 @@ public class SkyCreateFileCodeCreator extends WriteCommandAction.Simple {
 				}
 				xmlName = "fragment_" + xmlName.toLowerCase();
 
-				view = generateView(viewName, xmlName, bizName, "jc.sky.view.SKYFragment");
 				xml = gennerateXml(xmlName);
+				view = generateView(viewName, xmlName, bizName, "jc.sky.view.SKYFragment");
 				break;
 			case 2: // dialog fragment
 				if (!viewName.endsWith("DialogFragment")) {
@@ -90,8 +94,8 @@ public class SkyCreateFileCodeCreator extends WriteCommandAction.Simple {
 				}
 				xmlName = "dialogfragment_" + xmlName.toLowerCase();
 
-				view = generateView(viewName, xmlName, bizName, "jc.sky.view.SKYDialogFragment");
 				xml = gennerateXml(xmlName);
+				view = generateView(viewName, xmlName, bizName, "jc.sky.view.SKYDialogFragment");
 				break;
 		}
 
@@ -99,15 +103,14 @@ public class SkyCreateFileCodeCreator extends WriteCommandAction.Simple {
 
 		// reformat class
 		JavaCodeStyleManager styleManager = JavaCodeStyleManager.getInstance(mProject);
+		styleManager.shortenClassReferences(xml);
+		styleManager.optimizeImports(xml);
 		styleManager.shortenClassReferences(biz);
 		styleManager.shortenClassReferences(view);
-		styleManager.shortenClassReferences(xml);
 		styleManager.optimizeImports(biz);
-		styleManager.optimizeImports(xml);
 		styleManager.optimizeImports(view);
-		styleManager.optimizeImports(view.getContainingFile());
-		new ReformatCodeProcessor(mProject, new PsiFile[] { view, biz, xml }, null, false).runWithoutProgress();
 
+//		new ReformatCodeProcessor(mProject, new PsiFile[] { view, biz, xml }, null, false).runWithoutProgress();
 	}
 
 	private void runAdapter(String className) {
@@ -125,10 +128,9 @@ public class SkyCreateFileCodeCreator extends WriteCommandAction.Simple {
 				}
 				xmlName = "item_" + xmlName.toLowerCase() + "_item";
 
-				view = generateAdapter(viewName, xmlName, "jc.sky.view.adapter.recycleview.SKYRVAdapter");
-
 				xml = gennerateXml(xmlName);
 
+				view = generateAdapter(viewName, xmlName);
 				break;
 			case 1: // adapter more
 				if (!viewName.endsWith("Adapter")) {
@@ -138,11 +140,11 @@ public class SkyCreateFileCodeCreator extends WriteCommandAction.Simple {
 				String topXml = "item_" + xmlName.toLowerCase() + "_top";
 				String bottomXml = "item_" + xmlName.toLowerCase() + "_bottom";
 
-				view = generateAdapterMore(viewName, topXml, bottomXml, "jc.sky.view.adapter.recycleview.SKYRVAdapter");
-
 				xml = gennerateXml(topXml);
+
 				xmlMore = gennerateXml(bottomXml);
 
+				view = generateAdapterMore(viewName, topXml, bottomXml);
 				break;
 		}
 
@@ -150,128 +152,119 @@ public class SkyCreateFileCodeCreator extends WriteCommandAction.Simple {
 		JavaCodeStyleManager styleManager = JavaCodeStyleManager.getInstance(mProject);
 		styleManager.shortenClassReferences(view);
 		styleManager.optimizeImports(xml);
-		if(xmlMore != null){
+		if (xmlMore != null) {
 			styleManager.optimizeImports(xmlMore);
 		}
 		styleManager.optimizeImports(view.getContainingFile());
-		new ReformatCodeProcessor(mProject, new PsiFile[] { view,xml,xmlMore }, null, false).runWithoutProgress();
+//		new ReformatCodeProcessor(mProject, new PsiFile[] { view, xml, xmlMore }, null, false).runWithoutProgress();
 	}
 
-	private PsiFile generateAdapterMore(String viewName, String xmlTopName, String xmlBottomName, String superName) {
+	private PsiFile generateAdapterMore(String viewName, String xmlTopName, String xmlBottomName) {
 		StringBuilder fileName = new StringBuilder(viewName);
-		fileName.append(".java");
 
-		StringBuilder contentText = new StringBuilder();
+		FileTemplate template = FileTemplateManager.getInstance(mProject).getInternalTemplate("SkyAdapterMoreTemplate");
+		Properties defaultProperties = FileTemplateManager.getInstance(mProject).getDefaultProperties();
+		Properties properties = new Properties(defaultProperties);
+		properties.setProperty("NAME", fileName.toString());
+		properties.setProperty("LAYOUT1", xmlTopName);
+		properties.setProperty("LAYOUT2", xmlBottomName);
 
-		// 包
-		contentText.append("import android.view.LayoutInflater;\n").append("import android.view.View;\n").append("import android.view.ViewGroup;\n\n").append("import jc.sky.view.SKYActivity;\n")
-				.append("import jc.sky.view.adapter.recycleview.SKYHolder;\n").append("import jc.sky.view.adapter.recycleview.SKYRVAdapter;\n\n");
-
-		// 类
-		contentText.append("public class ").append(viewName).append(" extends ").append(superName).append("<").append(viewName).append(".Model, SKYHolder> {\n\n");
-
-		contentText.append("\tpublic static final int\tTYPE_ONE\t= 1;\n\n");
-
-		contentText.append("\tpublic static final int\tTYPE_TWO\t= 2;\n\n");
-
-		contentText.append("\tpublic ").append(viewName).append("(SKYActivity SKYActivity) {\n" + "\t\tsuper(SKYActivity);\n" + "\t}\n\n");
-
-		contentText.append("\t@Override public int getCustomViewType(int position) {\n" + "\t\treturn getItem(position).type;\n" + "\t}\n\n");
-
-		contentText.append("\t@Override public SKYHolder newViewHolder(ViewGroup viewGroup, int type) {\n" + "\t\tView view;\n" + "\t\tSKYHolder skyHolder = null;\n" + "\t\tswitch (type) {\n"
-				+ "\t\t\tcase TYPE_ONE:\n" + "\t\t\t\t// 修改布局文件\n" + "\t\t\t\tview = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout." + xmlTopName + ", viewGroup, false);\n"
-				+ "\t\t\t\tskyHolder = new TopHolder(view);\n" + "\t\t\t\tbreak;\n" + "\t\t\tcase TYPE_TWO:\n" + "\t\t\t\t// 修改布局文件\n"
-				+ "\t\t\t\tview = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout." + xmlBottomName + ", viewGroup, false);\n" + "\t\t\t\tskyHolder = new BottomHolder(view);\n"
-				+ "\t\t\t\tbreak;\n" + "\t\t\tdefault:\n" + "\t\t\t\tbreak;\n" + "\t\t}\n" + "\t\treturn skyHolder;\n" + "\t}\n\n");
-
-		contentText.append("\tpublic class TopHolder extends SKYHolder<Model> {\n" + "\n" + "\t\tpublic TopHolder(View itemView) {\n" + "\t\t\tsuper(itemView);\n" + "\t\t}\n" + "\n"
-				+ "\t\t@Override public void bindData(Model model, int position) {\n" + "\n" + "\t\t}\n" + "\t}\n\n");
-
-		contentText.append("\tpublic class BottomHolder extends SKYHolder<Model> {\n" + "\n" + "\t\tpublic BottomHolder(View itemView) {\n" + "\t\t\tsuper(itemView);\n" + "\t\t}\n" + "\n"
-				+ "\t\t@Override public void bindData(Model model, int position) {\n" + "\n" + "\t\t}\n" + "\t}\n\n");
-
-		contentText.append("\tpublic static class Model {\n" + "\n" + "\t\tpublic int type;\n" + "\t}\n");
-
-		contentText.append("}");
-
-		PsiFile psiClass = PsiFileFactory.getInstance(mProject).createFileFromText(fileName.toString(), JavaFileType.INSTANCE, contentText.toString());
-		PsiManager.getInstance(mProject).findDirectory(psiDirectory.getVirtualFile()).add(psiClass);
-
-		return psiClass;
+		try {
+			PsiElement psiElement = FileTemplateUtil.createFromTemplate(template, fileName.toString(), properties, psiDirectory);
+			return psiElement.getContainingFile();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 
-	private PsiFile generateAdapter(String viewName, String xmlName, String superName) {
+	private PsiFile generateAdapter(String viewName, String xmlName) {
 		StringBuilder fileName = new StringBuilder(viewName);
-		fileName.append(".java");
 
-		StringBuilder contentText = new StringBuilder();
-		// 包
-		contentText.append("import android.view.LayoutInflater;\n").append("import android.view.View;\n").append("import android.view.ViewGroup;\n\n").append("import jc.sky.view.SKYActivity;\n")
-				.append("import jc.sky.view.adapter.recycleview.SKYHolder;\n").append("import jc.sky.view.adapter.recycleview.SKYRVAdapter;\n\n");
+		FileTemplate template = FileTemplateManager.getInstance(mProject).getInternalTemplate("SkyAdapterTemplate");
+		Properties defaultProperties = FileTemplateManager.getInstance(mProject).getDefaultProperties();
+		Properties properties = new Properties(defaultProperties);
+		properties.setProperty("NAME", fileName.toString());
+		properties.setProperty("LAYOUT", xmlName);
 
-		// 类
-		contentText.append("public class ").append(viewName).append(" extends ").append(superName).append("<").append(viewName).append(".Model, ").append(viewName).append(".ItemHolder> {\n\n");
-
-		contentText.append("\tpublic ").append(viewName).append("(SKYActivity SKYActivity) {\n" + "\t\tsuper(SKYActivity);\n" + "\t}\n\n");
-
-		contentText.append(
-				"\t@Override public ItemHolder newViewHolder(ViewGroup viewGroup, int type) {\n" + "\t\t// 修改布局文件\n" + "\t\tView view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout."
-						+ xmlName + ", viewGroup, false);\n" + "\t\tItemHolder itemHolder = new ItemHolder(view);\n" + "\t\treturn itemHolder;\n" + "\t}\n\n");
-
-		contentText.append("\tpublic class ItemHolder extends SKYHolder<Model> {\n" + "\n" + "\t\tpublic ItemHolder(View itemView) {\n" + "\t\t\tsuper(itemView);\n" + "\t\t}\n" + "\n"
-				+ "\t\t@Override public void bindData(Model model, int position) {\n" + "\n" + "\t\t}\n" + "\t}\n\n");
-
-		contentText.append("\tpublic static class Model {}\n");
-		contentText.append("}");
-
-		PsiFile psiClass = PsiFileFactory.getInstance(mProject).createFileFromText(fileName.toString(), JavaFileType.INSTANCE, contentText.toString());
-		PsiManager.getInstance(mProject).findDirectory(psiDirectory.getVirtualFile()).add(psiClass);
-
-		return psiClass;
+		try {
+			PsiElement psiElement = FileTemplateUtil.createFromTemplate(template, fileName.toString(), properties, psiDirectory);
+			return psiElement.getContainingFile().getOriginalFile();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 	private PsiFile generateView(String viewName, String xmlName, String bizName, String superName) {
 		StringBuilder fileName = new StringBuilder(viewName);
-		fileName.append(".java");
 
-		StringBuilder contentText = new StringBuilder();
-		if(superName.endsWith("SKYActivity")) {
-			contentText.append("import jc.sky.SKYHelper;\n");
-			contentText.append("import jc.sky.display.SKYIDisplay;\n");
+		StringBuilder importText = new StringBuilder();
+		if (superName.endsWith("SKYActivity")) {
+			importText.append("import jc.sky.SKYHelper;\n");
+			importText.append("import jc.sky.display.SKYIDisplay;\n");
 		}
-		contentText.append("import android.os.Bundle;\n").append("import ").append(superName).append(";\n").append("import jc.sky.view.SKYBuilder;\n");
+		importText.append("import android.os.Bundle;\n").append("import ").append(superName).append(";\n").append("import jc.sky.view.SKYBuilder;\n");
 
-		contentText.append("\npublic class ").append(viewName).append(" extends ").append(superName).append("<").append(bizName).append("> {\n");
+		StringBuilder methodText = new StringBuilder();
 
-		if(superName.endsWith("SKYActivity")){
-			contentText.append("\n\tpublic static final void intent() {\n");
-			contentText.append("\t\tSKYHelper.display(SKYIDisplay.class).intent("+viewName+".class);\n");
-			contentText.append("\t}\n");
-		}else {
-			contentText.append("\n\tpublic static final "+viewName+" getInstance() {\n");
-			contentText.append("\t\t"+viewName+" "+viewName.toLowerCase() +" = new "+viewName+"();\n");
-			contentText.append("\t\tBundle bundle = new Bundle();\n");
-			contentText.append("\t\t"+viewName.toLowerCase()+".setArguments(bundle);\n");
-			contentText.append("\t\treturn "+viewName.toLowerCase()+";\n");
-			contentText.append("\t}\n");
+		if (superName.endsWith("SKYActivity")) {
+			methodText.append("\tpublic static final void intent() {\n");
+			methodText.append("\t\tSKYHelper.display(SKYIDisplay.class).intent(" + viewName + ".class);\n");
+			methodText.append("\t}");
+		} else {
+			methodText.append("\tpublic static final " + viewName + " getInstance() {\n");
+			methodText.append("\t\t" + viewName + " " + viewName.toLowerCase() + " = new " + viewName + "();\n");
+			methodText.append("\t\tBundle bundle = new Bundle();\n");
+			methodText.append("\t\t" + viewName.toLowerCase() + ".setArguments(bundle);\n");
+			methodText.append("\t\treturn " + viewName.toLowerCase() + ";\n");
+			methodText.append("\t}");
 		}
 
-		// ADD method
-		contentText.append("\n    @Override protected SKYBuilder build(SKYBuilder initialSKYBuilder) {\n" + "        initialSKYBuilder.layoutId(R.layout." + xmlName + ");\n"
-				+ "        return initialSKYBuilder;\n" + "    }\n");
-		// ADD method
-		contentText.append("\n    @Override protected void initData(Bundle savedInstanceState) {\n" + "    } \n");
-
+		StringBuilder methodDialogText = new StringBuilder();
 		if (superName.endsWith("SKYDialogFragment")) {
 			// ADD method
-			contentText.append("\n    @Override protected int getSKYStyle() {\n" + "        return 0;\n" + "    }\n");
+			methodDialogText.append("\n@Override protected int getSKYStyle() {\n" + "        return 0;\n" + "    }\n");
 		}
 
-		contentText.append("}");
-		PsiFile psiClass = PsiFileFactory.getInstance(mProject).createFileFromText(fileName.toString(), JavaFileType.INSTANCE, contentText.toString());
-		PsiManager.getInstance(mProject).findDirectory(psiDirectory.getVirtualFile()).add(psiClass);
+		FileTemplate template = FileTemplateManager.getInstance(mProject).getInternalTemplate("SkyViewTemplate");
+		Properties defaultProperties = FileTemplateManager.getInstance(mProject).getDefaultProperties();
+		Properties properties = new Properties(defaultProperties);
+		properties.setProperty("NAME", fileName.toString());
+		properties.setProperty("IMPORT", importText.toString());
+		properties.setProperty("SUPPORT", superName + "<" + bizName + ">");
+		properties.setProperty("METHOD_INTENT", methodText.toString());
+		properties.setProperty("LAYOUT", xmlName);
+		properties.setProperty("METHOD_DIALOG", methodDialogText.toString());
 
-		return psiClass;
+		try {
+			PsiElement psiElement = FileTemplateUtil.createFromTemplate(template, fileName.toString(), properties, psiDirectory);
+			return psiElement.getContainingFile();
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	private PsiFile generateBiz(String bizName, String viewName) {
+		StringBuilder fileName = new StringBuilder(bizName);
+
+		FileTemplate template = FileTemplateManager.getInstance(mProject).getInternalTemplate("SkyBizTemplate");
+		Properties defaultProperties = FileTemplateManager.getInstance(mProject).getDefaultProperties();
+		Properties properties = new Properties(defaultProperties);
+		properties.setProperty("NAME", fileName.toString());
+		properties.setProperty("SUPPORT", viewName);
+
+		try {
+			PsiElement psiElement = FileTemplateUtil.createFromTemplate(template, fileName.toString(), properties, psiDirectory);
+			return psiElement.getContainingFile();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return null;
 	}
 
 	private PsiFile gennerateXml(String xmlName) {
@@ -299,27 +292,7 @@ public class SkyCreateFileCodeCreator extends WriteCommandAction.Simple {
 		return psiClass;
 	}
 
-	private PsiFile generateBiz(String bizName, String viewName) {
-
-		StringBuilder fileName = new StringBuilder(bizName);
-		fileName.append(".java");
-
-		StringBuilder contentText = new StringBuilder();
-
-		contentText.append("import jc.sky.core.SKYBiz;\n").append("import android.os.Bundle;\n");
-		contentText.append("\npublic class ").append(bizName).append(" extends jc.sky.core.SKYBiz<").append(viewName).append("> {\n");
-
-		// add method
-		contentText.append("\n    @Override protected void initBiz(Bundle bundle) {\n" + "        super.initBiz(bundle);\n" + "    }\n");
-		contentText.append("}");
-
-		PsiFile psiClass = PsiFileFactory.getInstance(mProject).createFileFromText(fileName.toString(), JavaFileType.INSTANCE, contentText.toString());
-		PsiManager.getInstance(mProject).findDirectory(psiDirectory.getVirtualFile()).add(psiClass);
-
-		return psiClass;
-	}
-
-	private String defaultXMLContent(){
+	private String defaultXMLContent() {
 		StringBuilder contentText = new StringBuilder();
 		contentText.append("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n");
 		contentText.append("<RelativeLayout xmlns:android=\"http://schemas.android.com/apk/res/android\"\n");
